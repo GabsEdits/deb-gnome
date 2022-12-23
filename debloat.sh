b4dcf5250a2bc536073c3bae417f248ff47a1e43
gabs <110247388+GabsEdits@users.noreply.github.com>
2022-12-23 16:44:35 +0200

    Update debloat.sh

--- debloat.sh
+++ debloat.sh
@@ -1,2 +1,7 @@
-## Removing Games
-nala uninstall # When I well install debian I well put the pages here
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./auto.sh" 2>&1
+  exit 1
+fi
+# START
+nala remove aisleriot five-or-more four-in-a-row gnome-klotski gnome-mahjongg gnome-mines gnome-nibbles quadrapassel gnome-chess lghtsoff  gnome-robots gnome-sudoku swell-foop gnome-tetravex
